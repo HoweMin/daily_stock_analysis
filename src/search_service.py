a00bde350bc061981ce2b75b6d472f097218e8af
Haomin Bai <134840093+HoweMin@users.noreply.github.com>
2026-09-18 15:05:28 +0800

    Change strict_freshness to False in search_service

--- src/search_service.py
+++ src/search_service.py
@@ -4496,7 +4496,7 @@ class SearchService:
                     ),
                     'desc': '公司公告',
                     'tavily_topic': 'news',
-                    'strict_freshness': True,
+                    'strict_freshness': False,
                 },
                 {
                     'name': 'risk_check',
